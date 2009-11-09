9734576b8574c53102150a3181a9209691c15ec1
Odin Hørthe Omdal <odin.omdal@gmail.com>
2009-11-09 19:00:47 +0100

    pygments-lexer for herleg patch-visning

--- framlegg/views.py
+++ framlegg/views.py
@@ -1,3 +1,4 @@
+# vim: ts=4 sts=4 expandtab sw=4
 import sys
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
@@ -33,7 +34,7 @@ def patch_make(request, doc_id):
 
     diff_string = ''.join(list(diff))
 
-    p = Patch(patch=diff_string, document=doc, written_by='test', created_by='odin')
+    p = Patch(text=diff_string, document=doc, written_by='test', created_by='odin', reason=request.POST['reason'])
     p.save()
 
     return HttpResponseRedirect(
@@ -44,12 +45,23 @@ def patch_view(request, doc_id, patch_id):
     doc = get_object_or_404(Document, pk=doc_id)
     p = get_object_or_404(Patch, pk=patch_id)
 
-    from framlegg.patch import patch
+    from framlegg.patch import patch, show_diff
     import difflib
-    newdoc = patch(p.patch, doc.text)
-    d = difflib.HtmlDiff().make_table(doc.text.splitlines(1), newdoc.splitlines(1))
+    try:
+        newdoc = patch(p.text, doc.text)
+        d = difflib.SequenceMatcher(None, doc.text, newdoc)
+        hilightdoc = show_diff(d)
+    except Exception:
+        hilightdoc = ""
+
+    from pygments import highlight
+    from pygments.lexers import DiffLexer
+    from pygments.formatters import HtmlFormatter
+
+    diff = highlight(p.text, DiffLexer(), HtmlFormatter())
+
 
     return render_to_response('framlegg/patch_view.html',
                               {'doc': doc, 'patch': p,
-                              'newdoc': newdoc, 'd':d},
+                              'newdoc': hilightdoc, 'diff': diff},
                               context_instance=RequestContext(request))
