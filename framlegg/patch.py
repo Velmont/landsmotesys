a0af60e480699fb26db802fcc4ec44e222e061b6
Odin Hørthe Omdal <odin.omdal@gmail.com>
2009-11-09 18:58:00 +0100

    show_diff-funksjon frå stackoverflow for å visa endring

--- framlegg/patch.py
+++ framlegg/patch.py
@@ -1,3 +1,4 @@
+# vim: ts=4 sts=4 expandtab sw=4
 import os
 import tempfile
 import subprocess
@@ -24,17 +25,18 @@ def patch(diff, file):
 
     if failure:
         f = open("%s.diff" %
-                 (os.path.join(tempdir, os.path.basename(filename))), "w")
-        f.write(diff)
+                 (os.path.join(tempdir, "feil")), "w")
+        f.write(diff.encode('utf-8'))
         f.close()
 
         # FIXME: This doesn't provide any useful error report on why the patch
         # failed to apply, which makes it hard to debug.  We might also want to
         # have it clean up if DEBUG=False
-        raise Exception(_("The patch to '%s' didn't apply cleanly. The temporary " +
-                          "files have been left in '%s' for debugging purposes.\n" +
-                          "`patch` returned: %s") %
-                        (filename, tempdir, patch_output))
+        return Exception("doh")
+        #raise Exception("The patch to '%s' didn't apply cleanly. The temporary " +
+        #                  "files have been left in '%s' for debugging purposes.\n" +
+        #                  "`patch` returned: %s" %
+        #                ("feil", tempdir, patch_output))
 
     import codecs
     f = codecs.open(newfile, "r", encoding="utf-8")
@@ -46,3 +48,32 @@ def patch(diff, file):
     os.rmdir(tempdir)
 
     return data
+
+import difflib
+def show_diff(seqm):
+    """Unify operations between two compared strings
+seqm is a difflib.SequenceMatcher instance whose a & b are strings"""
+    output= []
+
+    for opcode, a0, a1, b0, b1 in seqm.get_opcodes():
+        if opcode == 'equal':
+            output.append(seqm.a[a0:a1])
+
+        elif opcode == 'insert':
+            output.append("<ins>" + seqm.b[b0:b1] + "</ins>")
+
+        elif opcode == 'delete':
+            output.append("<del>" + seqm.a[a0:a1] + "</del>")
+            if seqm.a[a1:a1+1] == '\n':
+                output.append('\n')
+
+        elif opcode == 'replace':
+            output.append("<ins>" + seqm.b[b0:b1] + "</ins>" + "<del>" + seqm.a[a0:a1] + "</del>")
+            if seqm.a[a1:a1+1] == '\n':
+                output.append('\n')
+
+        else:
+            raise RuntimeError, "unexpected opcode"
+
+    return ''.join(output)
+
