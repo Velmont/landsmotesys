68a50ee35f1ba29842fcdfb2faeba84bf0bb55dc
Odin Hørthe Omdal <odin@thor.s0.no>
2009-11-16 18:10:52 +0100

    Laga start for dokumentforslag

--- framlegg/urls.py
+++ framlegg/urls.py
@@ -5,3 +5,7 @@ urlpatterns = patterns('framlegg.views',
     (r'^doc/(?P<doc_id>\d+)/$', 'document'),
     (r'^cat/(?P<cat_id>\d+)/$', 'cat_view'),
 )
+
+urlpatterns += patterns('',
+    (r'^doc/new/$', 'django.views.generic.simple.direct_to_template', {'template': 'framlegg/doc_new.html'}),
+)
