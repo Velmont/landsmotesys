76a2de8ebf667f7189b05be80aee90a0e82db3f8
Odin Horthe Omdal <odin.omdal@gmail.com>
2010-01-04 14:25:30 +0100

    AGPL-a heile programmet, gjer klar for rlz

--- framlegg/urls.py
+++ framlegg/urls.py
@@ -1,3 +1,20 @@
+# vim: ts=4 sts=4 expandtab sw=4 fileencoding=utf8
+# Copyright 2009, 2010 Nei til EU, Odin Hørthe Omdal
+#
+# This file is part of Landsmotesys.
+#
+# Landsmotesys is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Landsmotesys is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Landsmotesys.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf.urls.defaults import *
 from models import DocumentForm
 
